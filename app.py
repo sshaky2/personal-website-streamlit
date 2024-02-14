4a65683ba4573adb6f4ffba87c016894f8e59205
Shobhit Shakya <shobhit.shakya@gmail.com>
2024-02-14 01:04:28 -0500

    refactor

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ from PIL import Image
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Optimize Eyebrow", layout="wide")
 
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
 
 # Use local CSS
 def local_css(file_name):
