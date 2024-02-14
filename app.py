8f06ca4f0c14c8d9952e17c69c1d59565eeb83e5
Shobhit Shakya <shobhit.shakya@gmail.com>
2024-02-14 00:57:36 -0500

    debug

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ local_css("style/style.css")
 img_1 = Image.open("images/2021-03-12.jpg")
 img_2 = Image.open("images/133953700_1063355007518135_1256085435724720813_n.jpg")
 
-st.header('Optimize Eyebrow', divider='rainbow')
+st.header('Optimize Eyebrow')
 # ---- HEADER SECTION ----
 with st.container():
     # st.title("Optimize Eyebrow")
